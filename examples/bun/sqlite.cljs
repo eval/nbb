c717bd1b20c9a01f2941c89e3f411f56a01fd2c3
Gert Goet <gert@thinkcreate.dk>
2024-04-16 13:15:10 +0200

    Showing bundle issue
    $ bun run --bun nbb bundle sqlite.cljs -o out.mjs && bun build out.mjs
    --compile --outfile cli
      [47ms]  bundle  12 modules
      [43ms] compile  cli
    ~/src/nbb/examples/bun (patch-1 *=)
    $ ./cli
    error: Cannot find module "./nbb_deps.js" from "/$bunfs/root/cli"

--- examples/bun/sqlite.cljs
+++ examples/bun/sqlite.cljs
@@ -1,5 +1,13 @@
 (ns sqlite
-  (:require ["bun:sqlite$default" :as sqlite]))
+  (:require ["bun:sqlite$default" :as sqlite]
+            [honey.sql :as sql]))
+
+(def sqlmap {:select [:a :b :c]
+             :from   [:foo]
+             :where  [:= :foo.a "baz"]})
+
+(prn (sql/format sqlmap))
+
 
 (defn select []
   (let [db (new sqlite ":memory:")]
